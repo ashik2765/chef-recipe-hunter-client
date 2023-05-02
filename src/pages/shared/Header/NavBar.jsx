df01e73650f28db6e85fd2131964c99cd2b91d9e
Ashikur Rahman <ashikurrahman2765@gmail.com>
2023-05-02 12:50:12 +0600

    add error page and navbar

--- src/pages/shared/Header/NavBar.jsx
+++ src/pages/shared/Header/NavBar.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 
 const NavBar = () => {
     return (
-        <div>
-            <h2>This is navbar</h2>
-        </div>
+        <Container>
+
+            <Navbar collapseOnSelect expand="lg" bg="bs-warning-bg-subtle" variant="light">
+      <Container>
+      <Navbar.Brand className='fw-bolder' href="#home">Tabbakh</Navbar.Brand>
+
+        
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mx-auto">
+            <Nav.Link className='text-primary' to='/'>Home</Nav.Link>
+            <Nav.Link href="#about">Blog</Nav.Link>
+            <Nav.Link href="#career">Career</Nav.Link>
+            
+          </Nav>
+          <Nav className='flex align-items-center'>
+            <Nav.Link href="#deets">Profile</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+            <Button variant="secondary">Login</Button>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+        </Container>
     );
 };
 
--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layouts/Main";
 import Home from "../pages/Home/Home/Home";
+import ErrorPage from "../ErrorPage";
 
 const router = createBrowserRouter([
     {
         path:'/',
         element: <Main></Main>,
+        errorElement:<ErrorPage></ErrorPage>,
         children:[
             {
                 path:'/',
