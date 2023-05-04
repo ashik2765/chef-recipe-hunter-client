241878bc708677b6e0267b65413e10bb4b08b11b
Ashikur Rahman <ashikurrahman2765@gmail.com>
2023-05-04 21:02:48 +0600

    add spinner

--- src/AuthProvider.jsx
+++ src/AuthProvider.jsx
@@ -7,24 +7,28 @@ export const AuthContext = createContext(null)
 const auth = getAuth(app);
 const AuthProvider = ({children}) => {
     const [user,setUser] =useState(null);
+    const[loading,setLoading] = useState(true);
     
     
     const createUser = (email,password)=>{
+        setLoading(true);
         
         return createUserWithEmailAndPassword(auth,email,password,)
     }
     const signIn = (email,password)=>{
-        
+        setLoading(true);
         return signInWithEmailAndPassword(auth,email,password,)
     }
 
     const logOut = () =>{
         return signOut(auth)
+        setLoading(true)
     }
     useEffect(()=>{
       const unsubscribe = onAuthStateChanged(auth,loggedUser =>{
             
             setUser(loggedUser);
+            setLoading(false);
         })
         return ()=>{
             unsubscribe();
@@ -32,9 +36,11 @@ const AuthProvider = ({children}) => {
     },[])
     const authInfo ={
         user,
+        loading,
         createUser,
         signIn,
-        logOut
+        logOut,
+
     
     }
     return (
